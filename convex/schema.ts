b1298438dcd84208aae1f2df48aefe82ee0ca967
Rakesh Puppala <rakeshpuppala2591.com>
2024-11-08 21:35:08 -0800

    added shadcn with sidebar and dropdown menu

--- convex/schema.ts
+++ convex/schema.ts
@@ -2,6 +2,13 @@ import { defineSchema, defineTable } from "convex/server";
 import { v } from "convex/values";
 
 export default defineSchema({
+  users: defineTable({
+    username: v.string(),
+    image: v.string(),
+    clerkId: v.string(),
+    // Add any other user-related fields here
+  }).index("by_clerk_id", ["clerkId"]),
+
   messages: defineTable({
     sender: v.string(),
     content: v.string(),
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,29 +22,22 @@ export default function Home() {
   };
 
   return (
-    <>
-      <Authenticated>
-        <div>
-          {messages?.map((message, index) => (
-            <div key={index}>
-              <strong>{message.sender}</strong>: {message.content}
-            </div>
-          ))}
-          <form onSubmit={handleSubmit}>
-            <input
-              type="text"
-              name="message"
-              value={input}
-              id="message"
-              onChange={(e) => setInput(e.target.value)}
-            />
-            <button type="submit">Send</button>
-          </form>
+    <div>
+      {messages?.map((message, index) => (
+        <div key={index}>
+          <strong>{message.sender}</strong>: {message.content}
         </div>
-      </Authenticated>
-      <Unauthenticated>
-        <SignInButton />
-      </Unauthenticated>
-    </>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="message"
+          value={input}
+          id="message"
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit">Send</button>
+      </form>
+    </div>
   );
 }
